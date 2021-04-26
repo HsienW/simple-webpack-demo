f254b03190c0bc38d3561a380be66e5f8f07d2d8
Wallace H Wu <wallace_h_wu@wistron.com>
2021-04-26 18:09:19 +0800

    add simple webpack simulation

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const babylon = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const babel = require('@babel/core');
+
+function createAsset(filename) {
+}
+
+function createGraph(entry) {
+}
+
+createGraph('./components/entry.js');
